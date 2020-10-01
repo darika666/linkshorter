560eeaa2d7d3117b12bfb901cdd9ad383b83ee91
darika666 <begalievad@gmail.com>
2020-10-01 21:23:02 +0600

    initial commit

---
+++ src/link/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+
+class LinkForm(forms.Form):
+    link = forms.URLField()
+    link.widget.attrs.update({
+        'class': 'form-control',
+
+        'placeholder': "Please enter link"})
+
+
+
+
+
+
+
+
+
+
